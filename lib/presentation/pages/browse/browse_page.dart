060942f87a697754e29f2edbc0b07a9d045f7803
HttpAnimations <shawndouglasyt@gmail.com>
2026-07-10 22:47:30 -0700

    修复Browse页面缺少Clipboard导入
    Models标签页的复制按钮仍需services.dart导入

--- lib/presentation/pages/browse/browse_page.dart
+++ lib/presentation/pages/browse/browse_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../bloc/servers/servers_bloc.dart';
